bbe07ac12a94dab7c3e8a24c30cbf8e299dcc1fd
Amrita <ask4am11@gmail.com>
2022-08-19 19:05:02 +0530

    Screenshot n multiple windows/tabs

---
+++ src/test/java/Multiple_windows.java
@@ -0,0 +1,37 @@
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Multiple_windows {
+
+    public static void main(String[] args) throws IOException {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\vk sinha\\Downloads\\chromedriver_updated\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://rahulshettyacademy.com/angularpractice/");
+        driver.switchTo().newWindow(WindowType.WINDOW);
+
+        Set<String> handles= driver.getWindowHandles();
+        Iterator<String> it= handles.iterator();
+        String parent_id =it.next();
+        String child_id = it.next();
+        driver.switchTo().window(child_id);
+
+        driver.get("https://www.rahulshettyacademy.com/");
+        String courseName= driver.findElements(By.cssSelector("a[href*='https://courses.rahulshettyacademy.com/p']")).get(1).getText();
+
+        driver.switchTo().window(parent_id);
+        driver.findElement(By.name("name")).sendKeys(courseName);
+
+        //screenshot for particular element
+        WebElement email=driver.findElement(By.name("name"));
+        File downaloadableFile=email.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(downaloadableFile,new File("logo.png"));
+    }
+}
